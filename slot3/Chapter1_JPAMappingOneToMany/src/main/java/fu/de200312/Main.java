aa1467e095e2a3ae53c390d28929d0c2333e6b32
VoNguyenKhanh <richn.k123a@gmail.com>
2026-09-15 16:22:09 +0700

    TODO2.1 - TODO2.4 : Entity Department, Employee, Gender; Owning side trong Employee; Inverse side trong Department; Helper method dong bo 2 chieu

---
+++ slot3/Chapter1_JPAMappingOneToMany/src/main/java/fu/de200312/Main.java
@@ -0,0 +1,6 @@
+package fu.de200312;
+public class Main {
+    static void main() {
+
+    }
+}
---
+++ slot3/Chapter1_JPAMappingOneToMany/src/main/java/fu/de200312/pojo/Gender.java
@@ -0,0 +1,7 @@
+package fu.de200312.pojo;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
