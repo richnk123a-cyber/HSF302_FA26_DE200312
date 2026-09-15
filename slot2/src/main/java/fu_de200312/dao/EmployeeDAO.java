225c8753543603637281267895e0e8c0e186819d
VoNguyenKhanh <richn.k123a@gmail.com>
2026-09-15 18:12:24 +0700

    TODO 0.3 - CREATE: EmployeeDAO.save(Employee e)

---
+++ slot2/src/main/java/fu_de200312/dao/EmployeeDAO.java
@@ -0,0 +1,30 @@
+﻿package fu_de200312.dao;
+
+import fu_de200312.pojo.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class EmployeeDAO {
+    private final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("hsf302FU");
+
+    public void save(Employee e) {
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+
+            em.persist(e);
+
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw ex;
+        } finally {
+            em.close();
+        }
+    }
+}
