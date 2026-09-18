caf0010afe3f5fb81170f7bfa43d03b11df207a0
VoNguyenKhanh <richn.k123a@gmail.com>
2026-09-18 13:55:25 +0700

    (TODO 5.2) tao moi quan he cua Employee

--- slot4/JPA_Mapping_ManyToMany/src/main/java/fu_de200312/pojo/Employee.java
+++ slot4/JPA_Mapping_ManyToMany/src/main/java/fu_de200312/pojo/Employee.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "employees")
@@ -32,6 +34,14 @@ public class Employee {
 
     @Transient
     private int yearsOfService;
+
+    @ManyToMany
+    @JoinTable(
+            name = "employee_project",
+            joinColumns = @JoinColumn(name = "employee_id"),
+            inverseJoinColumns = @JoinColumn(name = "project_id")
+    )
+    private Set<Project> projects = new HashSet<>();
 
     public Employee() {
     }
