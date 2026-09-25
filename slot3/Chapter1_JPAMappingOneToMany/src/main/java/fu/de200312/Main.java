31485acceaac3cc1451e4eebe0a219bb68ede6bb
VoNguyenKhanh <richn.k123a@gmail.com>
2026-09-25 14:09:24 +0700

    chore(setup): init spring boot project and configure sql server
    Thêm .gitignore, cấu hình datasource, ddl-auto=create, show-sql
    
    Refs: TODO 1

--- slot3/Chapter1_JPAMappingOneToMany/src/main/java/fu/de200312/Main.java
+++ slot3/Chapter1_JPAMappingOneToMany/src/main/java/fu/de200312/Main.java
@@ -1,12 +1,9 @@
 package fu.de200312;
 
+import fu.de200312.dao.DepartmentDAO;
 import fu.de200312.pojo.Department;
 import fu.de200312.pojo.Employee;
 import fu.de200312.pojo.Gender;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -14,43 +11,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        EntityManagerFactory emf =
-                Persistence.createEntityManagerFactory("hsf302FU");
+        Department dept = new Department("Marketing", "Ha Noi");
+        Employee emp = new Employee("test2@company.com", "Test", Gender.OTHER,
+                new BigDecimal("1000"), LocalDate.now());
+        Employee e2 = new Employee("bc@company.com", "B", Gender.FEMALE,
+                new BigDecimal("1200"), LocalDate.of(2022, 2, 1));
+        Employee e3 = new Employee("cd@company.com", "C", Gender.OTHER,
+                new BigDecimal("1500"), LocalDate.of(2022, 3, 1));
+        dept.addEmployee(emp);
+        dept.addEmployee(e2);
+        dept.addEmployee(e3);
 
-        EntityManager em = emf.createEntityManager();
-
-        try {
-            Department dept = new Department("IT", "Ha Noi");
-
-            Employee emp = new Employee(
-                    "test@company.com",
-                    "Test",
-                    Gender.OTHER,
-                    new BigDecimal("1000"),
-                    LocalDate.now()
-            );
-
-            dept.addEmployee(emp);
-
-            em.getTransaction().begin();
-
-            em.persist(dept);
-
-            em.getTransaction().commit();
-
-            System.out.println("Department ID: " + dept.getId());
-            System.out.println("Employee ID: " + emp.getId());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-
-            if (em.getTransaction().isActive()) {
-                em.getTransaction().rollback();
-            }
-
-        } finally {
-            em.close();
-            emf.close();
-        }
+        DepartmentDAO deptDAO = new DepartmentDAO();
+        deptDAO.save(dept);
+        System.out.println("Đã thêm: " + dept.getName());
     }
 }
